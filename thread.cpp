ceffcfaac72391550f8c69bf85361fe984c4cf39
mohanbera <immohanbera777@gmail.com>
2021-09-19 21:26:12 +0530

    more about thread added

--- thread.cpp
+++ thread.cpp
@@ -111,4 +111,118 @@ int main()
 }
 
 
+// There is another way of passing the parameter without copying 
+// and not sharing memory between the threads. We can use move():
 
+#include <iostream>
+#include <thread>
+#include <string.h>
+using namespace std;
+
+void displayName(string name, int age){
+    cout << name+" "+to_string(age) << endl;
+    name += " Bera";
+}
+
+int main()
+{
+    string name = "Mohan";
+    thread t1(&displayName, move(name), 22);
+    t1.join();
+    cout << "the name is "+name<< endl;
+    cout << "the name length is " << name.length() << endl;
+    return 0;
+}
+
+// output will be 
+// Mohan 22
+// the name is 
+// the name length is 0
+
+// Thread id
+// We can get id information using this_thread::get_id():
+
+int main()
+{
+    std::string s = "Name Surname";
+    std::thread t(&thread_function, std::move(s));
+    std::cout << "main thread message = " << s << std::endl;
+
+    std::cout << "main thread id = " << std::this_thread::get_id() << std::endl;
+    std::cout << "child thread id = " << t.get_id() << std::endl;
+
+    t.join();
+    return 0;
+}
+
+// output 
+
+/*
+thread function message is = Kathy Perry
+main thread message =
+main thread id = 1208
+child thread id = 5224
+*/
+
+// How many threads?
+// The thread library provides the suggestion for the number of threads:
+
+int main()
+{
+    std::cout << "Number of threads = " 
+              <<  std::thread::hardware_concurrency() << std::endl;
+    return 0;
+}
+
+// output 
+// Number of threads = 2
+
+
+// lambda functions
+// Since we're dealing with C11, let's take a little break for "lambda".
+
+// We can replace the thread_function() with lambda function (anonymous function) like this:
+
+int main()
+{
+    std::thread t([]()
+    {
+        std::cout << "thread function\n";
+    }
+    );
+    std::cout << "main thread\n";
+    t.join();     // main thread waits for t to finish
+    return 0;
+}
+
+// Note that we are writing inline code and passing into another function which is a thread constructor.
+
+// The lambda expression is a series of statements enclosed in braces, 
+// prefixed with [], called lambda introducer or capture specification 
+// which tells the compiler we're creating a lambda function, in our case, 
+// taking no argument. So, in essence, we're using [](){} as a task, 
+// and assigning it to our thread.
+
+// if we want to access local variable inside the 
+// lamda function then the variable nedd to be static other wise compile error
+
+#include <iostream>
+#include <thread>
+#include <string.h>
+using namespace std;
+
+
+int main()
+{
+    // static things can be accessed within lamda funtion
+    static string name = "Name Surname";
+
+    thread t1([](){
+        cout << "Hello World" + name << endl;
+    });
+
+    t1.join();
+    return 0;
+}
+
+// 
